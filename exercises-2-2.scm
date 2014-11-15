fc5f05952a09d3e3c681040b7f3121f6c1081ee2
Simon Woolf <simon.woolf@econsultancy.com>
2014-11-15 18:44:00 +0000

    up to 2.20

--- exercises-2-2.scm
+++ exercises-2-2.scm
@@ -5,4 +5,44 @@
       (last-pair (cdr l))))
 
 ; 2.18
+(define (reverse l)
+  (if (= 1 (length l))
+      l
+      (append (reverse (cdr l))
+              (list (car l)))))
 
+; or better still, handle length 0 lists
+; (needed for count-change):
+(define (reverse l)
+  (if (= 0 (length l))
+      '()
+      (append (reverse (cdr l))
+              (list (car l)))))
+
+; 2.19
+(define (first-denomination coin-values)
+  (car (last-pair coin-values)))
+
+(define (except-first-denomination coin-values)
+  (reverse (cdr (reverse coin-values))))
+
+(define (no-more? coin-values)
+  (= 0 (length coin-values)))
+
+; Order shouldn't affect it, no
+; though might affect efficiency
+
+; 2.20
+(define (same-parity x . y)
+  (define (same-parity-helper x . y)
+  (if (= 0 (length y))
+      null
+  (if (= (modulo x 2)
+         (modulo (car y) 2))
+      (cons (car y)
+            (apply same-parity-helper x (cdr y)))
+      (apply same-parity-helper x (cdr y)))))
+  (cons x (apply same-parity-helper x y)))
+
+; will's suggestion: signature of same-parity-helper
+; can just be (x y), so you don't have to use apply
