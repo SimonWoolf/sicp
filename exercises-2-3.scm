7e642f75cb780a682d7ffdf92baff31995a388bf
Simon Woolf <simon.woolf@econsultancy.com>
2015-02-26 17:12:13 +0000

    refactor 2.54 with any? and all?; 2.55

--- exercises-2-3.scm
+++ exercises-2-3.scm
@@ -21,3 +21,38 @@
           (my-equal? (cdr a) (cdr b))))
     (else #f)
     ))
+
+; Or using an all? helper I just wrote:
+(define (all? proc list)
+  (if
+    (null? list)
+    #t
+    (and (proc (car list))
+         (all? proc (cdr list)))
+    ))
+
+(define (any? proc list)
+  (if
+    (null? list)
+    #f
+    (or (proc (car list))
+        (all? proc (cdr list)))
+    ))
+
+(define (my-equal? a b)
+  (cond
+    ((all? number? (list a b)) (= a b))
+    ((all? symbol? (list a b)) (eq? a b))
+    ; need any? rather than all? for null else if one is
+    ; null and the other isn't it'll be caught by the
+    ; list branch and try to call car on it
+    ((any? null? (list a b)) (eq? a b))
+    ((all? list? (list a b))
+     (and (my-equal? (car a) (car b))
+          (my-equal? (cdr a) (cdr b))))
+    (else #f)
+    ))
+
+; 2.55
+; ''abracadabra evaluates to '(quote abracadabra)
+
