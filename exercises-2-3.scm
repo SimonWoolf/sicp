7716393323b786530b2619e85b9f683cf326b336
Simon Woolf <simon.woolf@econsultancy.com>
2015-03-01 19:12:30 +0000

    start 2.58

--- exercises-2-3.scm
+++ exercises-2-3.scm
@@ -111,3 +111,37 @@
 (define (multiplicand p) (if (null? (cdddr p)) ; only 2 args
                              (caddr p)
                              (cons '* (cddr p))))
+
+; 2.58 - infixness!
+(define (sum? x)
+  (and (pair? x) (eq? (cadr x) '+)))
+
+(define (product? x)
+  (and (pair? x) (eq? (cadr x) '*)))
+
+(define (addend s) (car s))
+(define (augend s) (caddr s))
+
+(define (multiplier p) (car p))
+(define (multiplicand p) (caddr p))
+
+; start with old make-sum & make-product from q, as can assume only 2 args
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1) (number? a2))
+         (+ a1 a2))
+        (else (list a1 '+ a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0)
+             (=number? m2 0))
+         0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) 
+         (* m1 m2))
+        (else (list '* m1 m2))))
+
+
+
