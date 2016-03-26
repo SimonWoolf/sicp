03cb8be8a49ae664dd3811426985b88f5e83eef4
Simon Woolf <simon@ably.io>
2016-03-26 22:15:55 +0000

    2.3

--- exercises-2-3.scm
+++ exercises-2-3.scm
@@ -54,7 +54,7 @@
     ))
 
 ; 2.55
-; ''abracadabra evaluates to '(quote abracadabra) = ('quote 'abracadabra)
+; ''abracadabra evaluates to '(quote abracadabra) = (list 'quote 'abracadabra)
 
 ; 2.56
 ((exponential? exp)
@@ -139,9 +139,208 @@
          0)
         ((=number? m1 1) m2)
         ((=number? m2 1) m1)
-        ((and (number? m1) (number? m2)) 
+        ((and (number? m1) (number? m2))
          (* m1 m2))
         (else (list '* m1 m2))))
 
 
+; 2.59
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((element-of-set? (car s1) s2)
+         (union-set (cdr s1) s2))
+        (else (cons (car s1) (union-set (cdr s1) s2)))))
 
+; 2.60
+; element-of-set is unchanged
+; intersection-set is also unchanged
+
+(define adjoin-set cons)
+
+(define union-set append)
+
+; would use this if you want O(1) adjoin-set and O(n) union-set
+; at the expense of space. eg if adding things to a set is very common
+
+; 2.61
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set))
+         (cons x set))
+        ((= x (car set))
+         set)
+        (else
+          (cons (car set) (adjoin-set x (cdr set))))))
+; only needs to go as far as the right insertion point
+
+; 2.62
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1)) (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1 (union-set
+                              (cdr set1)
+                              (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set
+                              (cdr set1)
+                              set2)))
+                  ((< x2 x1)
+                   (cons x2 (union-set
+                              set1
+                              (cdr set2)))))))))
+
+; 2.63
+; They produce the same results.
+;
+; But they don't have the same complexity, because the first one does an appen
+; at each non-null vertix which is O(size of left side). So it's O(n^2) rather
+; than O(n). (will it be n^2? might be better than that. not sure). So right
+; one is more efficient even though it's less intuitive
+
+; 2.64
+
+(define (list->tree elements)
+  (car (partial-tree
+        elements (length elements))))
+
+; I rewrote it to be easier for me to understand
+(define (partial-tree elts n)
+  (cond ((= n 0) (cons '() elts))
+    (else
+      (define left-size
+        (quotient (- n 1) 2)) ; integer division
+      (define left-tree-pair
+        (partial-tree elts left-size))
+      (define left-tree
+        (car left-tree-pair))
+      (define non-left-elements
+        (cdr left-tree-pair))
+      (define centre
+        (car non-left-elements))
+      (define right-tree-pair
+        (partial-tree (cdr non-left-elements) (- n left-size 1))) ; -1 for the centre one
+      (define right-tree
+        (car right-tree-pair))
+      (define remaining-els
+        (cdr right-tree-pair))
+      (cons (make-tree centre left-tree right-tree) remaining-els)
+      )))
+
+; recursively takes the first n elements, splits it into three (left half,
+; centre right half), and calls partial-tree on the left and right halves. (not
+; quite since the remaining-els trick saves calculating the right half elements
+; up front, but that's the basic idea)
+;
+; Since integer division rounds down, it prefers things being on the right side:
+;   5
+;  / \
+; 1   9
+; |  / \
+; 3 7  11
+;
+; Order of growth: partial-tree is called once per node, so O(n)
+
+; 2.65
+; can't we just do this?
+(define (union-set s1 s2)
+  (foldl adjoin-set s1 (tree->list s2)))
+; except this is O(n.log(n)) since adjoin-set is O(log(n)). hmm
+;
+; Could always just convert to ordered lists and back, but not sure that's in
+; the spirit of the question either :/
+(define (union-set s1 s2)
+  (list->tree (union-set-ordered-list (tree->list s1) (tree->list s2))))
+(define (intersection-set s1 s2)
+  (list->tree (intersection-set-ordered-list (tree->list s1) (tree->list s2))))
+; :(
+; but it is technically O(n), since all four ops are O(n), and each done once
+; TODO: think of other solutinos
+
+; 2.66
+; just element-of-set with (key ...) wrapped around every entry retreival
+(define (lookup given-key set-of-records)
+  (cond ((null? set-of-records) false)
+        ((= given-key (key (entry set-of-records))) true)
+        ((< given-key (key (entry set-of-records)))
+         (lookup
+           given-key
+           (left-branch set-of-records)))
+        ((> given-key (key (entry set-of-records)))
+         (lookup
+           given-key
+           (right-branch set-of-records)))))
+
+; 2.67
+'(A D A B B C A)
+
+; 2.68
+(define (encode-symbol symbol tree)
+  (cond ((leaf? tree)
+         '())
+        ((memq symbol (symbols (left-branch tree)))
+         (cons 0 (encode-symbol symbol (left-branch tree))))
+        ((memq symbol (symbols (right-branch tree)))
+         (cons 1 (encode-symbol symbol (right-branch tree))))
+        (else (error "symbol not in tree: " symbol ", tree: " (symbols tree)))))
+
+; Addendum re 2.72: would probably be better if searching the right branch
+; first? since when making the tree, weight(left-branch <=
+; weight(right-branch), so more efficient to search the branch with higher
+; weight first
+
+; 2.69
+(define (successive-merge leaf-set)
+  (if (= 1 (length leaf-set))
+    (car leaf-set)
+    (successive-merge (adjoin-set (make-code-tree (car leaf-set)
+                          (cadr leaf-set))
+          (cddr leaf-set)))))
+
+; 2.70
+(define 1950s (generate-huffman-tree '((A 2) (BOOM 1) (GET 2) (JOB 2) (NA 16) (SHA 3) (YIP 9) (WAH 1))))
+; > 1950s
+'(((leaf NA 16)
+   ((leaf YIP 9)
+    (((leaf A 2) ((leaf WAH 1) (leaf BOOM 1) (WAH BOOM) 2) (A WAH BOOM) 4)
+     ((leaf SHA 3) ((leaf JOB 2) (leaf GET 2) (JOB GET) 4) (SHA JOB GET) 7)
+     (A WAH BOOM SHA JOB GET)
+     11)
+    (YIP A WAH BOOM SHA JOB GET)
+    20)
+   (NA YIP A WAH BOOM SHA JOB GET)
+   36))
+
+
+(encode '(GET A JOB SHA NA NA NA NA NA NA NA NA GET A JOB SHA NA NA NA NA NA NA NA NA WAH YIP YIP YIP YIP YIP YIP YIP YIP YIP SHA BOOM) 1950s)
+'(1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 0 0 1 1 1 1 0 1 1 1 0 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 0 1 1 1 0 1 1 0 1 1)
+; 84 bits
+; A fixed-length code would take 3 bits per word for 36 words = 108 bits. So we save 20 bits
+
+; 2.71
+; Tree will have one leaf and one node on every level
+;    /\
+;   /\
+;  /\
+; /\
+; etc
+;
+; Most frequent symbol: 1 bit
+; Least frequent symbol: height of the tree = n-1 bits
+
+
+; 2.72
+; order of growth
+; = O(number of nodes to search * length of symbol list at each node)
+; = O(height of tree * length of symbol list)
+; For worst case (eg 2.71), each of those is n, so O(n^2)
+; For best case (balanced tree), height is log(n), so \Omega(n.log(n))
+;
+; for 2.71-worst-case, order of encoding for most frequent symbol depends on
+; whether it's on the left or the right branch. If the left, it's O(1). If the
+; right, O(n). For the least-frequent symbol, it's O(n^2) per previous para.
+;
+; could optimise by making encode-symbol search right branch first, as that has
+; weight >= left branch -- see addendum to 2.68
